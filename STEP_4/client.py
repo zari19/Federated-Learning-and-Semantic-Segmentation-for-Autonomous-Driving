45ca36f581e5212e5e13d6998aae6518a9ed5649
zari19 <144258993+zari19@users.noreply.github.com>
2023-09-07 14:19:12 +0200

    Add files via upload

---
+++ STEP_4/client.py
@@ -0,0 +1,251 @@
+import copy
+import torch
+import torch.nn.functional as F
+import os
+import numpy as np
+from PIL import Image
+from torch import optim, nn
+from collections import defaultdict
+from torch.utils.data import DataLoader
+from torchvision import models
+from utils.utils import get_scheduler
+import matplotlib.pyplot as plt
+from utils.utils import HardNegativeMining, MeanReduction
+from torch import distributed
+import torchvision.transforms
+from utils.dist_utils import initialize_distributed, setup, find_free_port
+import torch.optim.lr_scheduler as lr_scheduler
+from utils.loss import SelfTrainingLoss
+
+device = torch.device( 'cuda' if torch. cuda. is_available () else 'cpu')
+
+class Client:
+
+    def __init__(self, args, dataset, model, test_client=False):
+        self.args = args
+        self.dataset = dataset
+        self.name = self.dataset.client_name
+        self.model = model
+        self.train_loader = DataLoader(self.dataset, batch_size=self.args.bs, shuffle=True, drop_last=True) \
+            if not test_client else None
+        self.test_loader = DataLoader(self.dataset, batch_size=self.args.bs, shuffle=False)
+        #self.writer = writer
+        self.criterion = nn.CrossEntropyLoss(ignore_index=255, reduction='none')
+        self.reduction = HardNegativeMining() if self.args.hnm else MeanReduction()
+        
+    def __str__(self):
+        return self.name
+
+
+    def get_model(self):
+        return self.model
+
+
+    @staticmethod
+    def update_metric(metrics, outputs, labels, cur_step):
+        _, prediction = outputs.max(dim=1)
+        labels = labels.cpu().numpy()
+        prediction = prediction.cpu().numpy()
+        # print(f'update_metric_prediction_type = {type(prediction)}')
+        # print(f'update_metric_prediction_shape = {prediction.shape}')
+
+        # pred = prediction[0,:,:]
+        # plt.imshow(pred)
+        # plt.savefig('trial_imgs/pred{}.png'.format(cur_step))
+
+        metrics.update(labels, prediction)
+
+    def _get_outputs(self, images, model):
+        if self.args.model == 'deeplabv3_mobilenetv2':
+            return self.model(images)['out']
+        if self.args.model == 'resnet18':
+            return self.model(images)
+        raise NotImplementedError
+
+    def get_optimizer(self, net, lr, wd, momentum):
+      optimizer = torch.optim.SGD(net.parameters(), lr=lr, weight_decay=wd, momentum=momentum)
+      return optimizer
+
+    def loss_function(self):
+      loss_function = nn.CrossEntropyLoss()
+      return loss_function
+
+    def calc_losses(self, images, labels, student_model, teacher_model):
+      if self.args.model == 'deeplabv3_mobilenetv2':
+          outputs = self._get_outputs(images, student_model)
+          _, pred = outputs.max(dim=1)
+          
+          loss_module = SelfTrainingLoss(conf_th=0.5, fraction=0.66, ignore_index=255, lambda_selftrain=1)
+          loss_module.set_teacher(teacher_model)
+          
+          pseudo_lab = loss_module.get_pseudo_lab(outputs, images, return_mask_fract=False, model=None)
+          loss = loss_module.forward(outputs, images)
+          
+          dict_calc_losses = {'loss_tot': loss}
+      else:
+          raise NotImplementedError
+
+      return dict_calc_losses, outputs
+
+      
+    def handle_grad(self, loss_tot):
+        pass
+
+    def calc_loss_fed(dict_losses):
+        return dict_losses
+      
+    def clip_grad(self):
+        pass
+
+    def generate_update(self):
+        return copy.deepcopy(self.model.state_dict())
+
+
+    def _configure_optimizer(self, params):
+          if self.args.optimizer == 'SGD':
+              optimizer = optim.SGD(params, lr=self.args.lr, momentum=self.args.momentum,
+                                    weight_decay=self.args.weight_decay, nesterov=self.args.nesterov)
+          else:
+              optimizer = optim.Adam(params, lr=self.args.lr, weight_decay=self.args.weight_decay)
+          scheduler = get_scheduler(self.args, optimizer,
+                                    max_iter=10000 * self.args.num_epochs * len(self.loader))
+          return optimizer, scheduler
+
+    def handle_log_loss(self, dict_all_epoch_losses, dict_losses_list):
+
+        for n, l in dict_all_epoch_losses.items():
+
+            dict_all_epoch_losses[n] = torch.tensor(l).to(device)
+            #dict_losses_list[n].append(dict_all_epoch_losses[n])
+        return dict_all_epoch_losses, dict_losses_list
+
+
+    def run_epoch(self, cur_epoch, optimizer, metrics, scheduler, student_model, teacher_model):
+        """
+        This method locally trains the model with the dataset of the client. It handles the training at mini-batch level
+        :param cur_epoch: current epoch of training
+        :param optimizer: optimizer used for the local training
+        """
+        dict_all_epoch_losses = defaultdict(lambda: 0)
+
+        for cur_step, (images, labels) in enumerate(self.train_loader):
+            # TODO: missing code here!
+            images = images.to(device, dtype=torch.float32)
+            labels = labels.to(device, dtype=torch.long)
+            # print(f'train_images_shape = {images.shape}')
+            # print(f'train_output_shape = {labels.shape}')
+
+            optimizer.zero_grad()
+            dict_calc_losses, outputs = self.calc_losses(images, labels, student_model, teacher_model)
+            #print(f'outputs after calculate_loss = {outputs.shape}')
+
+            dict_calc_losses['loss_tot'].backward()
+            self.handle_grad(dict_calc_losses['loss_tot'])
+
+            self.clip_grad()
+            optimizer.step()
+            scheduler.step()
+
+            if cur_epoch == self.args.num_epochs - 1:
+              
+              self.update_metric(metrics, outputs, labels, cur_step)
+
+            print_string = ""
+            for name, l in dict_calc_losses.items():
+                  if type(l) != int:
+                      dict_all_epoch_losses[name] += l.detach().item()
+                  else:
+                      dict_all_epoch_losses[name] += l
+
+
+        for name, l in dict_all_epoch_losses.items():
+          dict_all_epoch_losses[name] /= len(self.train_loader)
+          print_string += f"{name}={'%.3f' % dict_all_epoch_losses[name]}, "
+          print(print_string)
+
+        return dict_all_epoch_losses
+            
+        
+
+    def train(self, metrics, student_model, teacher_model):
+        """
+        This method locally trains the model with the dataset of the client. It handles the training at epochs level
+        (by calling the run_epoch method for each local epoch of training)
+        :return: length of the local dataset, copy of the model parameters
+        """
+        num_train_samples = len(self.dataset)
+        #optimizer, scheduler = self._configure_optimizer(params)
+        dict_losses_list = defaultdict(lambda: [])
+        self.model.train()
+        #bn_dict_tmp = None
+        net = self.get_model()
+        opt = self.get_optimizer(net, lr=self.args.lr, wd=self.args.wd, momentum=self.args.m)
+        scheduler = lr_scheduler.StepLR(opt, step_size=5, gamma=0.1)
+        #self.model.train()
+        # TODO: missing code here!
+        for epoch in range(self.args.num_epochs):
+            # TODO: missing code here!
+            
+            dict_all_epoch_losses = self.run_epoch(epoch, optimizer = opt, metrics=metrics, scheduler=scheduler, student_model=student_model, teacher_model=teacher_model)
+            dict_all_epoch_losses, dict_losses_list = self.handle_log_loss(dict_all_epoch_losses, dict_losses_list)
+
+        #metrics.synch(self.device)
+
+        update = self.generate_update()
+
+        return num_train_samples, update, dict_losses_list
+
+    def subs_bn_stats(self, domain, train_cl_bn_stats):
+        pass
+
+    def copy_bn_stats(self):
+        pass
+
+
+    def test2(self, metric):
+        """
+        This method tests the model on the local dataset of the client.
+        :param metric: StreamMetric object
+        """
+        print("Testing...")
+        self.model.eval()
+        class_loss = 0.0
+        ret_samples = []
+
+        # if loader is None:
+        #     loader = self.loader
+
+        with torch.no_grad():
+            for i, sample in enumerate(self.test_loader):
+              images, labels = sample
+              
+              images = images.to(device, dtype=torch.float32)
+              labels = labels.to(device, dtype=torch.long)
+              # print(f'test_images_shape = {images.shape}')
+              # print(f'test_output_shape = {labels.shape}')
+              outputs = self._get_outputs(images, models)
+              # print(f'test_output_shape = {outputs.shape}')
+              # print(f'test_output_shape = {labels.shape}')
+
+              loss = self.reduction(self.criterion(outputs, labels),labels)
+              class_loss += loss.item()
+
+              _, prediction = outputs.max(dim=1)
+              labels = labels.cpu().numpy()
+              prediction = prediction.cpu().numpy()
+              metric['test_same_dom'].update(labels, prediction)
+
+
+              if self.args.plot == "True":
+                  pred2 = prediction[0,:,:]  # Select the first image from the batch
+                  plt.imshow(pred2)
+                  plt.savefig('test_imgs/pred{}.png'.format(i))
+
+            class_loss = torch.tensor(class_loss).to(device)
+            print(f'class_loss = {class_loss}')
+            class_loss = class_loss / len(self.test_loader)
+
+        return class_loss, ret_samples
+
+      
+
---
+++ STEP_4/main.py
@@ -0,0 +1,300 @@
+import os
+import json
+from collections import defaultdict
+from utils.utils import setup_pre_training, load_from_checkpoint
+import torch
+from torch.utils.data import DataLoader
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.optim as optim
+from torchvision.models import resnet18
+from utils.print_stats import print_stats
+import datasets.ss_transforms as sstr
+import datasets.np_transforms as nptr
+from torch import nn
+from client import Client
+from datasets.femnist import Femnist
+from server import Server
+from utils.args import get_parser
+from datasets.idda import IDDADataset
+from models.deeplabv3 import deeplabv3_mobilenetv2
+from utils.stream_metrics import StreamSegMetrics, StreamClsMetrics
+from utils.utils import setup_env
+from utils.client_utils import setup_clients
+from time import sleep
+from tqdm import tqdm
+from google.colab import auth
+import gspread
+from google.auth import default
+device = torch.device( 'cuda' if torch. cuda. is_available () else 'cpu')
+
+
+def set_seed(random_seed):
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.cuda.manual_seed_all(random_seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def get_dataset_num_classes(dataset): #return dataset number of classes
+    if dataset == 'idda':
+        return 16
+    if dataset == 'femnist':
+        return 62
+    raise NotImplementedError
+
+
+def model_init(args): #selects the type of model
+    if args.model == 'deeplabv3_mobilenetv2':
+        return deeplabv3_mobilenetv2(num_classes=get_dataset_num_classes(args.dataset))
+    if args.model == 'resnet18':
+        model = resnet18()
+        model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        model.fc = nn.Linear(in_features=512, out_features=get_dataset_num_classes(args.dataset))
+        return model
+    if args.model == 'cnn':
+        # TODO: missing code here!
+        raise NotImplementedError
+    raise NotImplementedError
+
+
+def get_transforms(args): #perform data augmentation based on the model
+    # TODO: test your data augmentation by changing the transforms here!
+    if args.model == 'deeplabv3_mobilenetv2':
+        train_transforms = sstr.Compose([
+            #sstr.RandomResizedCrop((1920, 1080), scale=(1.0, 1.0)), #default  512, 928  #scale .5,2
+            sstr.ToTensor(),
+            #sstr.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        test_transforms = sstr.Compose([
+            sstr.ToTensor(),
+            #sstr.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+    elif args.model == 'cnn' or args.model == 'resnet18':
+        train_transforms = nptr.Compose([
+            nptr.ToTensor(),
+            nptr.Normalize((0.5,), (0.5,)),
+        ])
+        test_transforms = nptr.Compose([
+            nptr.ToTensor(),
+            nptr.Normalize((0.5,), (0.5,)),
+        ])
+    else:
+        raise NotImplementedError
+    return train_transforms, test_transforms
+
+
+def read_femnist_dir(data_dir):
+    data = defaultdict(lambda: {})
+    files = os.listdir(data_dir)
+    files = [f for f in files if f.endswith('.json')]
+    for f in files:
+        file_path = os.path.join(data_dir, f)
+        with open(file_path, 'r') as inf:
+            cdata = json.load(inf)
+        data.update(cdata['user_data'])
+    return data
+
+
+def read_femnist_data(train_data_dir, test_data_dir):
+    return read_femnist_dir(train_data_dir), read_femnist_dir(test_data_dir)
+
+
+def get_datasets(args): #get access to datasets in root/idda
+
+    train_datasets = []
+    train_transforms, test_transforms = get_transforms(args)
+
+    if args.dataset == 'idda':
+        root = "/content/drive/MyDrive/DELIVERY/idda"  #maybe change path
+        psudo_labels_root = "/content/drive/MyDrive/DELIVERY/STEP_4/pseudo_label"
+
+        with open(os.path.join(root, 'train.json'), 'r') as f:
+            all_data = json.load(f)
+        for client_id in all_data.keys():
+            train_datasets.append(IDDADataset(root=root, list_samples=all_data[client_id], transform=train_transforms,
+                                              client_name=client_id))
+
+        with open(os.path.join(root, 'train.txt'), 'r') as f:
+            flag = True
+            train_idda = f.read().splitlines()
+            train_idda_dataset = IDDADataset(root=root, list_samples=train_idda, transform=train_transforms)
+            
+
+        with open(os.path.join(root, 'test_same_dom.txt'), 'r') as f:
+            test_same_dom_data = f.read().splitlines()
+            test_same_dom_dataset = IDDADataset(root=root, list_samples=test_same_dom_data, transform=test_transforms,
+                                                client_name='test_same_dom')
+        with open(os.path.join(root, 'test_diff_dom.txt'), 'r') as f:
+            test_diff_dom_data = f.read().splitlines()
+            test_diff_dom_dataset = IDDADataset(root=root, list_samples=test_diff_dom_data, transform=test_transforms,
+                                                client_name='test_diff_dom')
+        test_datasets = [test_same_dom_dataset, test_diff_dom_dataset]
+
+    elif args.dataset == 'femnist':
+        niid = args.niid
+        train_data_dir = os.path.join('data', 'femnist', 'data', 'niid' if niid else 'iid', 'train')
+        test_data_dir = os.path.join('data', 'femnist', 'data', 'niid' if niid else 'iid', 'test')
+        train_data, test_data = read_femnist_data(train_data_dir, test_data_dir)
+
+        train_transforms, test_transforms = get_transforms(args)
+
+        train_datasets, test_datasets = [], []
+
+        for user, data in train_data.items():
+            train_datasets.append(Femnist(data, train_transforms, user))
+        for user, data in test_data.items():
+            test_datasets.append(Femnist(data, test_transforms, user))
+
+    else:
+        raise NotImplementedError
+
+    return train_datasets, train_idda_dataset, test_datasets
+
+
+def set_metrics(args):
+    num_classes = get_dataset_num_classes(args.dataset)
+    if args.model == 'deeplabv3_mobilenetv2':
+        metrics = {
+            'eval_train': StreamSegMetrics(num_classes, 'eval_train'),
+            'test_same_dom': StreamSegMetrics(num_classes, 'test_same_dom'),
+            'test_diff_dom': StreamSegMetrics(num_classes, 'test_diff_dom')
+        }
+    elif args.model == 'resnet18' or args.model == 'cnn':
+        metrics = {
+            'eval_train': StreamClsMetrics(num_classes, 'eval_train'),
+            'test': StreamClsMetrics(num_classes, 'test')
+        }
+    else:
+        raise NotImplementedError
+    return metrics
+
+
+def gen_clients(args, train_datasets, test_datasets, model):
+    clients = [[], []]
+    for i, datasets in enumerate([train_datasets, test_datasets]):
+        for ds in datasets:
+            clients[i].append(Client(args, ds, model, test_client=i == 1))
+    return clients[0], clients[1]
+
+
+def main():
+    
+    parser = get_parser()
+    args = parser.parse_args()
+    set_seed(args.seed)
+
+    def weight_train_loss(losses):
+        """Function that weights losses over train round, taking only last loss for each user"""
+        fin_losses = {}
+        c = list(losses.keys())[0]
+        loss_names = list(losses[c]['loss'].keys())
+        for l_name in loss_names:
+            tot_loss = 0
+            weights = 0
+            for _, d in losses.items():
+                tot_loss += d['loss'][l_name][-1] * d['num_samples']
+                weights += d['num_samples']
+            fin_losses[l_name] = tot_loss / weights
+        return fin_losses
+
+    def _get_outputs(images):
+        if args.model == 'deeplabv3_mobilenetv2':
+            return model(images)['out']
+        if args.model == 'resnet18':
+            return model(images)
+        raise NotImplementedError
+
+    def load_checkpoints(PATH):
+        checkpoint = torch.load(PATH)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch = checkpoint['epoch']
+        loss = checkpoint['loss']
+
+    def get_pseudo_label(model):
+        print("Model evaluation...")
+        model.eval()
+
+        for cur_step, (images, labels) in enumerate(idda_dataset):
+
+            images = images.to(device, dtype=torch.float32)
+            labels = labels.to(device, dtype=torch.long)
+
+            outputs = _get_outputs(images)
+            _, prediction = outputs.max(dim=1)
+            labels = labels.cpu().numpy()
+            prediction = prediction.cpu().numpy()
+
+            pseudo_label = prediction[0,:,:]
+            plt.imshow(pseudo_label)
+            plt.savefig('pseudo_label/{}.png'.format(idda_dataset.dataset.list_samples[cur_step]))
+            
+
+    last_scores = defaultdict(lambda: defaultdict(lambda: []))
+
+    print("Step 4... actually working")
+    print(f'Initializing model...')
+    model = model_init(args)  #select type of model from the comand above
+    model.cuda()
+    print('Done.')
+
+    print('Generate datasets...')
+    train_datasets, train_idda_dataset, test_datasets = get_datasets(args)
+    idda_dataset = DataLoader(train_idda_dataset, batch_size=args.bs, shuffle=False)
+    print('Done.')
+
+    metrics = set_metrics(args)
+    train_clients, test_clients = gen_clients(args, train_datasets, test_datasets, model)
+    server = Server(args, train_clients, test_clients, model, metrics)
+
+
+    optimizer = optim.SGD(model.parameters(), lr=0.005, momentum=0.9)
+    
+
+    
+    load_checkpoints(PATH = "/content/drive/MyDrive/DELIVERY/STEP_4/checkpoint/model.pt")
+
+    student_model = model
+    teacher_model = model
+
+    #get_pseudo_label(model = teacher_model)
+    
+    t_rounds = 2
+    for r in tqdm(range(args.num_rounds)):
+            
+            loss = server.train(metrics['eval_train'], student_model = student_model, teacher_model = teacher_model)
+            print( f"ROUND {r + 1}/{args.num_rounds}: Training {args.clients_per_round} Clients...")
+
+            train_score = metrics['eval_train'].get_results()
+            print(train_score)
+
+            round_losses = weight_train_loss(loss)
+            print(round_losses)
+            for name, l in round_losses.items():
+                        print(f"R-{name}: {l}, step={r + 1}")
+            metrics['eval_train'].reset()
+            if args.modality4 == "2":
+                print("modality = 2")
+                server.update_model()
+            print(f'round numero ={r}')
+            if args.modality4 == "3" and r % t_rounds ==0:
+                print("modality = 3")
+                server.update_model()
+            #teacher_model = student_model
+
+    print("Train completed")
+
+    server.test2(test_clients, metrics, model)
+    test_score = metrics['test_same_dom'].get_results()
+
+    print(test_score) 
+
+    print("Job completed!!")
+
+if __name__ == '__main__':
+    main()
